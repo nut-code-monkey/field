64a591500a73af2ab4df26c42677089a0bf49b96
Max Lunin <nut.code.monkey@gmail.com>
2017-05-12 23:28:05 +0300

    Initial Commit

---
+++ field/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  field
+//
+//  Created by Max Lunin on 5/12/17.
+//  Copyright © 2017 Max Lunin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
